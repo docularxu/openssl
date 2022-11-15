c975b7a35708a639b983de36a2e10ed64d4c3112
Guodong Xu <guodong.xu@linaro.org>
2022-11-15 14:44:24 +0800

    Add a built-in provider loadbalance
    Add a built-in provider, which has name 'loadbalance'.
    To use the load-balance capability, it's required to load
    'loadbalance' provider into the application's libctx.
    
    And when fetching, need to specify property string
    "provider=loadbalance"
    
    Signed-off-by: Guodong Xu <guodong.xu@linaro.org>

--- crypto/provider_predefined.c
+++ crypto/provider_predefined.c
@@ -8,12 +8,14 @@
  */
 
 #include <openssl/core.h>
+#include <openssl/core_names.h>
 #include "provider_local.h"
 
 OSSL_provider_init_fn ossl_default_provider_init;
 OSSL_provider_init_fn ossl_base_provider_init;
 OSSL_provider_init_fn ossl_null_provider_init;
 OSSL_provider_init_fn ossl_fips_intern_provider_init;
+OSSL_provider_init_fn ossl_lb_provider_init;
 #ifdef STATIC_LEGACY
 OSSL_provider_init_fn ossl_legacy_provider_init;
 #endif
@@ -26,6 +28,7 @@ const OSSL_PROVIDER_INFO ossl_predefined_providers[] = {
     { "legacy", NULL, ossl_legacy_provider_init, NULL, 0 },
 # endif
     { "base", NULL, ossl_base_provider_init, NULL, 0 },
+    { PREFIX_LOAD_BALANCING_PROV_NAME, NULL, ossl_lb_provider_init, NULL, 0 },
     { "null", NULL, ossl_null_provider_init, NULL, 0 },
 #endif
     { NULL, NULL, NULL, NULL, 0 }
